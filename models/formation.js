407dc2ef26acdb6855c1c55ebfd5ba0390b842dc
Zegthor91 <zegthorthechazz@gmail.com>
2026-02-04 17:25:12 +0100

    Création des fichiers formation et hobby dans le dossier models

---
+++ models/formation.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const formationSchema = new mongoose.Schema(
+    {
+        titre: {type: String, required: true},
+        company: {type: String, required: true},
+        startDate: {type: String, default: ''},
+        endDate: {type: String, default: ''},
+        description: {type: String, default: ''},
+        order: {type: Number, default: 0},
+
+    },
+    { timestamps: true}
+);
+
+module.exports = mongoose.model('Formation', formationSchema);
---
+++ models/hobby.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const hobbySchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        details: {type: String, default: ''},
+        order: {type: Number, default: 0}
+    },
+    { timeStramps: true}
+);
+
+module.exports = mongoose.model('Hobby', hobbySchema);
